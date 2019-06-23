2b866526dd40b32e3250c09758b0d0e11823ac66
Matt Aiken <mattaiken@hotmail.co.uk>
2019-06-23 13:59:18 +0100

    Initial tests for LetterButton component plus utility data-test function

---
+++ src/components/LetterButton.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../../utilities/utilities';
+import LetterButton from './LetterButton';
+
+const testProps = {
+  value: 'a',
+  updateText: () => {}
+};
+
+describe('Letter Button component', () => {
+  let component;
+  beforeEach(() => {
+    component = shallow(<LetterButton {...testProps} />);
+  });
+
+  it('Should render without errors', () => {
+    const button = findByTestAttr(component, 'letterButton');
+    expect(button.length).toBe(1);
+  });
+});
