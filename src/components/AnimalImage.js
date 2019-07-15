2b951a2906620eae036faf5324b1edb2a2a995f8
Matt Aiken <mattaiken@hotmail.co.uk>
2019-07-15 13:18:44 +0100

    refactor

--- src/components/AnimalImage.js
+++ src/components/AnimalImage.js
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AnimalImage = props => {
+  const { image, title, currentlySelected } = props;
   return (
-    <div className="image" data-test="animalImageComponent">
-      <img src={props.image} alt="Animal" data-test="animalImage" />
+    <div
+      className={`image ${currentlySelected === title ? 'show' : 'hide'}`}
+      data-test="animalImageComponent"
+    >
+      <img src={image} id={title} alt="Animal" data-test="animalImage" />
     </div>
   );
 };
 
 AnimalImage.propTypes = {
-  image: PropTypes.string.isRequired
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  currentlySelected: PropTypes.string.isRequired
 };
 
 export default AnimalImage;
